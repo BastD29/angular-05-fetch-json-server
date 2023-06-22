5aabb0f8bc685a5377e9d491b0ec3474d8610f2d
Bastien Daniel <bastien.daniel@rawfish.com>
2023-06-22 18:14:12 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
 
+export interface User {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-json-server';
+  // users!: any[];
+  users!: User[];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
